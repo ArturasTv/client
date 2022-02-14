eb253c36fd64f049b9b182466bf1158b3ae2a3ec
ArturasTv <arturastvaronas@inbox.lt>
2022-02-15 01:59:00 +0200

    Creating item component

---
+++ src/App/components/item/item.jsx
@@ -0,0 +1,25 @@
+import styles from "./item.module.scss";
+import { GrMoreVertical } from "react-icons/gr";
+const Item = ({ task }) => {
+  return (
+    <div className={styles["item"]}>
+      <div className={styles["item-main"]}>
+        <div
+          className={styles[`item-status-${task.status ? "done" : "undone"}`]}
+        ></div>
+        <div className={styles["item-title"]}>{task.title}</div>
+        <div className={styles["item-info"]}>
+          <p title="Sukūrimo data">{task.created}</p>
+          <p className={styles["item-category"]} title="Kategorija">
+            {task.category}
+          </p>
+        </div>
+        <div className={styles["item-more"]}>
+          <GrMoreVertical title="Plačiau" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
