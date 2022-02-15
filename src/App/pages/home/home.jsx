607a9d5c737f7bcfa3995b0963d78b1d386fd387
ArturasTv <arturastvaronas@inbox.lt>
2022-02-16 01:19:30 +0200

    Creating home page component

---
+++ src/App/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import styles from "./home.module.scss";
+import ItemList from "../../components/item-list/item-list";
+
+const Home = () => {
+  return (
+    <div>
+      <ItemList />
+    </div>
+  );
+};
+
+export default Home;
