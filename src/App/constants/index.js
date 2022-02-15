fc51321a4d942389d87cd7dca8249f0255f196cd
ArturasTv <arturastvaronas@inbox.lt>
2022-02-16 01:20:37 +0200

    Adding menu items

--- src/App/constants/index.js
+++ src/App/constants/index.js
@@ -1,1 +1,12 @@
 export const sortValues = ["title", "category", "date"];
+
+export const menuItems = [
+  { title: "Pagrindinis", path: "/", key: "HOME", visible: true },
+  {
+    title: "Sukurti naują užduotį",
+    path: "/create",
+    key: "CREATE",
+    visible: true,
+  },
+  { title: "Užduotis", path: "/task", key: "TASK", visible: false },
+];
