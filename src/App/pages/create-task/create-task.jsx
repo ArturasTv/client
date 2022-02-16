05dcd5c7e644dfcd2ec9cc8b2066151cd19e8530
ArturasTv <arturastvaronas@inbox.lt>
2022-02-16 14:27:32 +0200

    Creating create task page

---
+++ src/App/pages/create-task/create-task.jsx
@@ -0,0 +1,11 @@
+import styles from "./create-task.module.scss";
+import CreateTaskForm from "../../components/create-task-form/create-task-form";
+const CreateTaskPage = () => {
+  return (
+    <div>
+      <CreateTaskForm />
+    </div>
+  );
+};
+
+export default CreateTaskPage;
