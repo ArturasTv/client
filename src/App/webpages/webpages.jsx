9315463d3a67d624223f7db6c8a8a65b877f8e1e
ArturasTv <arturastvaronas@inbox.lt>
2022-02-16 14:27:02 +0200

    Add functionality

--- src/App/webpages/webpages.jsx
+++ src/App/webpages/webpages.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "../layout/layout";
 import Home from "../pages/home/home";
 import TaskPage from "../pages/task/task-page";
+import CreateTaskPage from "../pages/create-task/create-task";
 const Webpages = () => {
   return (
     <Layout>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/Task/:id" element={<TaskPage />} />
+        <Route exact path="/task/:id" element={<TaskPage />} />
+        <Route exact path="/create" element={<CreateTaskPage />} />
       </Routes>
     </Layout>
   );
