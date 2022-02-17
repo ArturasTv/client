57297833441c76a97eb3c9718b710b0274446162
ArturasTv <arturastvaronas@inbox.lt>
2022-02-17 02:16:55 +0200

    Add functionality

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,12 @@
 import styles from "./App.module.scss";
+import Webpages from "./webpages/webpages";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Webpages />
+    </div>
+  );
 }
 
 export default App;
