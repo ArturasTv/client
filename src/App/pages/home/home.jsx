311b4ef826436dfb1d9838371dbec838e1b04fdd
ArturasTv <arturastvaronas@inbox.lt>
2022-02-17 02:20:29 +0200

    Add functionality

--- src/App/pages/home/home.jsx
+++ src/App/pages/home/home.jsx
@@ -1,12 +1,20 @@
 import styles from "./home.module.scss";
 import ItemList from "../../components/item-list/item-list";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getTasks } from "../../redux/actions/task-action";
+import Loading from "../../components/loading/loading";
+import { startLoading } from "../../redux/actions/loading-action";
 
 const Home = () => {
-  return (
-    <div>
-      <ItemList />
-    </div>
-  );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(startLoading());
+    dispatch(getTasks());
+  }, []);
+  const { tasks } = useSelector((state) => state.taskReducer);
+  const { isLoading } = useSelector((state) => state.loadingReducer);
+  return <div>{!tasks ? <Loading /> : <ItemList _tasks={tasks.tasks} />}</div>;
 };
 
 export default Home;
--- src/App/pages/task/task-page.jsx
+++ src/App/pages/task/task-page.jsx
@@ -1,13 +1,32 @@
 import styles from "./task-page.module.scss";
 import { useParams } from "react-router";
 import Task from "../../components/task/task";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getTask } from "./../../redux/actions/task-action";
+import { startLoading } from "../../redux/actions/loading-action";
+import Loading from "../../components/loading/loading";
+import { updateTask, deleteTask } from "./../../redux/actions/task-action";
 
 const TaskPage = () => {
+  const dispatch = useDispatch();
   const { id } = useParams();
+
+  const { isLoading } = useSelector((state) => state.loadingReducer);
+  const { task } = useSelector((state) => state.taskReducer);
+
+  const props = {
+    updateTask,
+    deleteTask,
+  };
+
+  useEffect(() => {
+    dispatch(startLoading());
+    dispatch(getTask(id));
+  }, []);
+
   return (
-    <div>
-      <Task />
-    </div>
+    <div>{!task ? <Loading /> : <Task item={task.task} {...props} />}</div>
   );
 };
 
