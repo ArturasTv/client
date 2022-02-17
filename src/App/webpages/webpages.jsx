2f166a4ff0bd111a8b605ee5b53961d78a990718
ArturasTv <arturastvaronas@inbox.lt>
2022-02-17 02:22:37 +0200

    Add functionality

--- src/App/webpages/webpages.jsx
+++ src/App/webpages/webpages.jsx
@@ -3,6 +3,7 @@ import Layout from "../layout/layout";
 import Home from "../pages/home/home";
 import TaskPage from "../pages/task/task-page";
 import CreateTaskPage from "../pages/create-task/create-task";
+import UpdateTaskPage from "../pages/update-task/update-task";
 const Webpages = () => {
   return (
     <Layout>
@@ -10,6 +11,7 @@ const Webpages = () => {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/task/:id" element={<TaskPage />} />
         <Route exact path="/create" element={<CreateTaskPage />} />
+        <Route exact path="/update" element={<UpdateTaskPage />} />
       </Routes>
     </Layout>
   );
