1ed5f198390bac854ced7d14e309f4427142f583
ArturasTv <arturastvaronas@inbox.lt>
2022-02-17 02:21:12 +0200

    Creating redux actions types

---
+++ src/App/redux/types/index.js
@@ -0,0 +1,8 @@
+export const GET_TASKS_SUCCESS = "GET_TASKS_SUCCESS";
+export const GET_TASK_SUCCESS = "GET_TASK_SUCCESS";
+
+export const SET_MESSAGE = "SET_MESSAGE";
+export const CLEAR_MESSAGE = "CLEAR_MESSAGE";
+
+export const START_LOADING = "START_LOADING";
+export const STOP_LOADING = "STOP_LOADING";
