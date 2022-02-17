871af5fe169c5786eca47e52843e8116deec9854
ArturasTv <arturastvaronas@inbox.lt>
2022-02-17 02:21:45 +0200

    Creating update task page

---
+++ src/App/pages/update-task/update-task.jsx
@@ -0,0 +1,22 @@
+import styles from "./update-task.module.scss";
+import UpdateTaskForm from "../../components/update-task-form/update-task-form";
+import { useSelector } from "react-redux";
+import Loading from "../../components/loading/loading";
+import { updateTask } from "../../redux/actions/task-action";
+const UpdateTaskPage = () => {
+  const { task } = useSelector((state) => state.taskReducer);
+
+  const props = { updateTask };
+
+  return (
+    <div>
+      {!task ? (
+        <Loading />
+      ) : (
+        <UpdateTaskForm item={{ ...task.task }} {...props} />
+      )}
+    </div>
+  );
+};
+
+export default UpdateTaskPage;
